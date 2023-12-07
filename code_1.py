df79a6104c51ded6c597d2e9fd41b5af914dbede
phonon222 <146646038+phonon222@users.noreply.github.com>
2023-12-07 17:48:04 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,38 @@
+if __name__ == "__main__":
+
+    main()
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+
+def main():
+def generate_random_data():
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+import random
+import random
+
+    return data
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    main()
+
+
+
+    data = generate_random_data()
+def generate_random_data():
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+        int[] data = new int[10];
+    }
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+        }
+    public static void main(String[] args) {
+}
+        }
