3510ec14a8c47a8fc84c9f0cffd292f0646754e3
phonon222 <146646038+phonon222@users.noreply.github.com>
2023-12-10 21:03:57 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,30 +1,69 @@
-        console.log("Random Number: " + item);
-    const data = [];
-
-function generateRandomData() {
-}
-    for (let i = 0; i < 10; i++) {
-
-function main() {
-    data.forEach(item => {
-    }
-main();
-    const data = generateRandomData();
-    return data;
-    });
-
-}
-        data.push(Math.floor(Math.random() * 100) + 1);if __name__ == "__main__":
     for item in data:
-    return data
-        print(f"Random Number: {item}")
 
-
-    main()
-    data = generate_random_data()
     data = [random.randint(1, 100) for _ in range(10)]
 def main():
+    data = generate_random_data()
+    return data
+
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+    main()
+
+if __name__ == "__main__":
+import random    main()
+def generate_random_data():
+if __name__ == "__main__":
+import random
+
+
+
+
+    return data
+    data = generate_random_data()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+import random
+    main()
 def generate_random_data():
 
 
-import random
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def main():
+    for item in data:
+    return data
+def generate_random_data():
+
+if __name__ == "__main__":
+    for item in data:
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+        print(f"Random Number: {item}")
+def main():
+    data = generate_random_data()
+import random    const data = [];
+    const data = generateRandomData();
+function generateRandomData() {
+
+
+}
+    for (let i = 0; i < 10; i++) {
+function main() {
+    });
+    return data;
+
+}
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
