470be7165eb94f3d3faa102cbf835e747acba942
phonon222 <146646038+phonon222@users.noreply.github.com>
2023-12-14 00:31:20 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,65 @@
+    main()
+def generate_random_data():
+import random
+        print(f"Random Number: {item}")
 def main():
 
-def generate_random_data():
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-    main()
     data = generate_random_data()
     for item in data:
 
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
+if __name__ == "__main__":
+    data = generate_random_data()
+import random
+    main()
+def main():
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    for item in data:
+
+if __name__ == "__main__":
+
+
+    return data
+if __name__ == "__main__":
+
+
+    main()
+
+        print(f"Random Number: {item}")
+import random
+def main():
+
+    data = generate_random_data()
+    for item in data:
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return dataif __name__ == "__main__":
+
+
+    for item in data:
+
+def main():
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def generate_random_data():
+    return data
+    data = generate_random_data()
+    main()
+    main()
+if __name__ == "__main__":
+    data = generate_random_data()
+
 
 import random
-    return data
+def generate_random_data():
+def main():
 
+        print(f"Random Number: {item}")
     data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    for item in data:
