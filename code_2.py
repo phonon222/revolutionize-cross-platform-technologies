3a46a2722b2dda152adbc4c90a64bc7fa58e6c8c
phonon222 <146646038+phonon222@users.noreply.github.com>
2023-12-16 02:54:09 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,26 @@
+    for item in data:
+    return data
+
+
+def main():
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+if __name__ == "__main__":
+
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+def generate_random_data():
+def main():
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+
+def generate_random_data():
+
+    return data
+    for item in data:
+
+import random
